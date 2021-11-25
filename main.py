1ed4b2e6dd883bbbdf2b7a34d80523eae94f7c5e
VictorKPetersen <victorkahlpetersen@gmail.com>
2021-11-25 21:14:04 +0100

    New Data Set

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from Descent import GradientDescent
 
 
 def main():
-    xValues = [0, 1, 2, 3, 4, 5]
-    yValues = [0, 2, 4, 6, 8, 10]
+    xValues = [0, 4, 7, 9, 15, 17, 21, 25, 31, 34, 36, 31]
+    yValues = [4, 2, 8, 12, 13, 16, 21, 19, 25, 20, 30, 35]
     GD1 = GradientDescent(0.0015, 100000, xValues, yValues)
     GD1.gradient_descent_regression()
 
