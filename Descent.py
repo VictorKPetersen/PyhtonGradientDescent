928c3f9d6ffdabee12e95509ef2f2df038de235e
VictorKPetersen <victorkahlpetersen@gmail.com>
2021-11-25 21:03:19 +0100

    Gradient descent implementation

--- Descent.py
+++ Descent.py
@@ -1,9 +1,37 @@
+import numpy as np
+
+
 class GradientDescent:
 
-    def __init__(self, learning_rate, iterations):
+    def __init__(self, learning_rate, iterations, x_data, y_data):
         self.learning_rate = learning_rate
         self.iterations = iterations
+        self.x = np.array(x_data)
+        self.y = np.array(y_data)
 
-        print(self.learning_rate, self.iterations)
+    def gradient_descent_regression(self):
+        a_current = 0
+        b_current = 0
+        n = len(self.x)
+        current_iteration = 0;
+        previous_cost = 0;
 
+        while current_iteration < self.iterations:
+            current_iteration += 1
 
+            y_prediction = a_current * self.x + b_current
+
+            cost = (1 / n) * sum([val ** 2 for val in (self.y - y_prediction)])
+
+            derivativeA = (2 / n) * sum(-self.x * (self.y - y_prediction))
+            derivativeB = (2 / n) * sum(-(self.y - y_prediction))
+
+            a_current = a_current - self.learning_rate * derivativeA
+            b_current = b_current - self.learning_rate * derivativeB
+
+            print(f"a: {a_current}, b: {b_current}, Cost: {cost}, Iteration: {current_iteration}")
+
+            if abs(cost - previous_cost) >= 5e-33:
+                previous_cost = cost
+            else:
+                break
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-#Victor Kahl Petersen - SOP Machine Learning - 3.D Rybners Teknisk Gymnasium
+# Victor Kahl Petersen - SOP Machine Learning - 3.D Rybners Teknisk Gymnasium
 from Descent import GradientDescent
+
+
+def main():
+    xValues = [0, 1, 2, 3, 4, 5]
+    yValues = [0, 2, 4, 6, 8, 10]
+    GD1 = GradientDescent(0.0015, 100000, xValues, yValues)
+    GD1.gradient_descent_regression()
+
+
 if __name__ == '__main__':
-    GD1 = GradientDescent(0.01, 1000)
-
-
-
-
-
-
-
+    main()
