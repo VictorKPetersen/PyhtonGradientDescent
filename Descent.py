d3e29b9990f14851012135ea0e5427707974e184
VictorKPetersen <victorkahlpetersen@gmail.com>
2021-11-25 18:28:04 +0100

    Testing of classes

---
+++ Descent.py
@@ -0,0 +1,9 @@
+class GradientDescent:
+
+    def __init__(self, learning_rate, iterations):
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+
+        print(self.learning_rate, self.iterations)
+
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Victor Kahl Petersen - SOP Machine Learning - 3.D Rybners Teknisk Gymnasium
+from Descent import GradientDescent
 if __name__ == '__main__':
-    print(__name__)
+    GD1 = GradientDescent(0.01, 1000)
+
 
 
 
