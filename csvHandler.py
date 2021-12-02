2cc951e35c10c0c31407c9402274b01483dc9479
VictorKPetersen <victorkahlpetersen@gmail.com>
2021-12-03 00:02:25 +0100

    CSV Test

--- csvHandler.py
+++ csvHandler.py
@@ -5,7 +5,11 @@ class CsvHandler:
 
     def __init__(self): pass
 
-    def readCSV(self): pass
+    def readCSV(self):
+        with open('\csvData\housePriceData.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            print(line)
 
 
 
