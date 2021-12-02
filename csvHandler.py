268d17b15b4ddc3013908912dc965a4b297a41a4
VictorKPetersen <victorkahlpetersen@gmail.com>
2021-12-03 00:13:37 +0100

    CSV Correct CSV File

--- csvHandler.py
+++ csvHandler.py
@@ -6,10 +6,10 @@ class CsvHandler:
     def __init__(self): pass
 
     def readCSV(self):
-        with open('\csvData\housePriceData.csv', 'r') as csv_file:
+        with open('housePriceData.csv', 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
-        for line in csv_reader:
-            print(line)
+            for line in csv_reader:
+                print(line)
 
 
 
